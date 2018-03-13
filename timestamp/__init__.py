13b65bf1690c62c18bf9ed47cdcae44b222a9f86
LeoComandini <leonardocomandini@hotmail.it>
2018-03-13 12:43:36 +0100

    add licence

--- timestamp/__init__.py
+++ timestamp/__init__.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python3
+#
+# Electrum Timstamp Plugin
+# Copyright (C) 2018 Leonardo Comandini
+#
+# It is subject to the license terms in the LICENSE file found in the top-level
+# directory of this distribution.
+#
+# based on
+# Electrum - lightweight Bitcoin client
+# Copyright (C) 2018 Thomas Voegtlin
+
 from electrum.i18n import _
 
 fullname = 'Timestamp'
